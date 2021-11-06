f3f315d4cacf3325809090e8136a2612c7b6bb4e
河田鷹介 <benefium@gmail.com>
2021-11-06 20:54:26 +0900

    hmmmm...

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -5,18 +5,6 @@
 #include <exception>
 #include <stdexcept>
 #include <stdlib.h>
-#include <sys/stat.h>
-
-bool    is_directory(const std::string& filename) {
-    struct stat	sb;
-
-    int rv = stat(filename.c_str(), &sb);
-    if (rv < 0 || S_ISDIR(sb.st_mode))
-    {
-        return true;
-    }
-    return false;
-}
 
 const std::string   read_all(
     const std::string& filename
@@ -25,17 +13,14 @@ const std::string   read_all(
     if (!ifs.is_open()) {
         throw std::string("Error: can't open a file: ") + filename;
     }
-    if (is_directory(filename)) {
-        ifs.close();
-        throw std::string("Error: is a directory: ") + filename;
-    }
+    // if (is_directory(filename)) {
+    //     throw std::string("Error: is a directory: ") + filename;
+    // }
     std::ostringstream receiver;
     receiver << ifs.rdbuf();
     if (!ifs.good() || ifs.fail()) {
-        ifs.close();
         throw std::string("Error: failed to read from the file: ") + filename;
     }
-    ifs.close();
     return std::string(receiver.str());
 }
 
@@ -45,25 +30,25 @@ void    replace_out(
     const std::string& str_to,
     std::ofstream& fout
 ) {
-    std::size_t body_len = body.length();
     std::size_t from_len = str_from.length();
-    std::size_t find_head = 0;
+    std::size_t find_from = 0;
     while (true) {
-        std::size_t found_head = body.find(str_from, find_head);
+        std::size_t found_head = body.find(str_from, find_from);
         if (found_head == std::string::npos) {
-            fout << body.substr(find_head, body_len - find_head);
             break;
         }
-        fout << body.substr(find_head, found_head - find_head);
+        // assumed that: find_from < found_head
+        fout << body.substr(find_from, found_head - find_from);
         if (!fout.good()) {
-            break;
+            return;
         }
         fout << str_to;
         if (!fout.good()) {
-            break;
+            return;
         }
-        find_head = found_head + from_len;
+        find_from = found_head + from_len;
     }
+    fout << body.substr(find_from);
 }
 
 void    replace(
@@ -74,16 +59,14 @@ void    replace(
     const std::string body = read_all(filename_original);
     const std::string filename_replaced = filename_original + ".replace";
     std::ofstream ofs(filename_replaced);
-    if (is_directory(filename_replaced)) {
-        throw std::string("Error: is a directory: ") + filename_replaced;
-    }
+    // if (is_directory(filename_replaced)) {
+    //     throw std::string("Error: is a directory: ") + filename_replaced;
+    // }
     if (!ofs.is_open()) {
         throw std::string("Error: failed to open output file: ") + filename_replaced;
     }
     replace_out(body, str_from, str_to, ofs);
-    bool out_succeeded = ofs.good();
-    ofs.close();
-    if (!out_succeeded) {
+    if (!ofs.good()) {
         throw std::string("Error: failed to write to output file: ") + filename_replaced;
     }
 }
